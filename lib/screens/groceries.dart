e82789919de9d48772b5908f386a1aa51d4e34b6
Vladimir Sukanica <vladimir.sukanica@zesium.com>
2024-02-02 17:46:50 +0100

    feat: Implement post and get requests

--- lib/screens/groceries.dart
+++ lib/screens/groceries.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/widgets/grocery.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 import 'package:shopping_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class GroceriesScreen extends StatefulWidget {
   const GroceriesScreen({super.key});
@@ -12,35 +15,61 @@ class GroceriesScreen extends StatefulWidget {
 }
 
 class _GroceriesScreenState extends State<GroceriesScreen> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https('flutter-course-5166b-default-rtdb.firebaseio.com',
+        'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> listData =
+        json.decode(response.body);
+    final List<GroceryItem> _loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (catItem) => catItem.value.title == item.value['category'])
+          .value;
+      _loadedItems.add(GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category));
+    }
+    setState(() {
+      _groceryItems = _loadedItems;
+    });
+  }
 
   void _addItem() async {
-    final result = await Navigator.of(context).push<GroceryItem>(
-        MaterialPageRoute(builder: (ctx) => const NewItem())
-    );
-    if (result == null) {
-      return;
-    } else {
-      setState(() {
-        _groceryItems.add(result);
-      });
-    }
+    await Navigator.of(context).push<GroceryItem>(
+        MaterialPageRoute(builder: (ctx) => const NewItem()));
+
+    _loadItems();
   }
 
   @override
   Widget build(BuildContext context) {
     Widget content = const Center(
-      child: Text('No items in list yet', style: TextStyle(fontSize: 20),),
+      child: Text(
+        'No items in list yet',
+        style: TextStyle(fontSize: 20),
+      ),
     );
-
 
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
           itemCount: _groceryItems.length,
-          itemBuilder: (ctx, index) =>
-              Dismissible(
+          itemBuilder: (ctx, index) => Dismissible(
                 key: ValueKey(_groceryItems[index].id),
-                background: Container(color: Colors.redAccent,),
+                background: Container(
+                  color: Colors.redAccent,
+                ),
                 onDismissed: (direction) {
                   setState(() {
                     _groceryItems.removeAt(index);
@@ -56,12 +85,11 @@ class _GroceriesScreenState extends State<GroceriesScreen> {
                   trailing: Text(_groceryItems[index].quantity.toString()),
                 ),
               ));
-
     }
 
-      return LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          return Scaffold(
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return Scaffold(
             appBar: AppBar(
               title: const Text('Your Groceries'),
               actions: [
@@ -80,8 +108,8 @@ class _GroceriesScreenState extends State<GroceriesScreen> {
             //   ),
             // ),
             // ),
-          );
-        },
-      );
-    }
+            );
+      },
+    );
+  }
 }
